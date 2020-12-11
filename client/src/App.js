7292c1169a9a1e0680cc547ac48169a72760e08a
czulueta <https://github.com/czulueta/weed.git>
2020-12-11 02:11:48 -0700

    setup first route on the frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,14 @@
-import React from "react"
+import React, {useState, useEffect} from "react"
+import axios from "axios"
 
 function App() {
+    const[bounties, setBountes] = useState([])
+
+    function getBounties() {
+        axios.get("/bounties")
+        .then(res => console.log(res))
+        .catch(err => console.log(err))
+    }
     return(
         <div>Bounty 3</div>
     )
